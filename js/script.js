ec8d1487b0de6a3b5a8b80f9c24c9a3ef7768387
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-13 15:44:08 +0300

    dfdxzmbg[

--- js/script.js
+++ js/script.js
@@ -856,9 +856,19 @@ ctx.fill();
 ctx.fillStyle = 'rgb(4, 1, 0)';
 
 ctx.beginPath();
-ctx.moveTo(110, 230);
+ctx.moveTo(110, 240);
+ctx.lineTo(120, 240);
 ctx.lineTo(120, 230);
-ctx.lineTo(120, 220);
-ctx.lineTo(110, 220);
 ctx.lineTo(110, 230);
+ctx.lineTo(110, 240);
+ctx.fill();
+
+ctx.fillStyle = 'rgb(4, 1, 0)';
+
+ctx.beginPath();
+ctx.moveTo(110, 240);
+ctx.lineTo(120, 240);
+ctx.lineTo(120, 230);
+ctx.lineTo(110, 230);
+ctx.lineTo(110, 240);
 ctx.fill();
